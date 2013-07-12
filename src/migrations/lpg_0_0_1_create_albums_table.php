444bbe159911c651fc2a90416e8b855c5cc4f4ca
JeroenG <jeroengjeroeng@gmail.com>
2013-07-12 23:47:33 +0200

    Migrations for albums and photos table

---
+++ src/migrations/lpg_0_0_1_create_albums_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlbumsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('albums', function(Blueprint $table)
+		{
+			$table->increments('album_id');
+			$table->string('album_name');
+			$table->string('album_description')->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
---
+++ src/migrations/lpg_0_0_2_create_photos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('photos', function(Blueprint $table)
+		{
+			$table->increments('photo_id');
+			$table->string('photo_name');
+			$table->string('photo_description')->nullable();
+			$table->string('photo_path');
+			$table->integer('album_id_fk')->unsigned();
+			$table->foreign('album_id_fk')->references('album_id')->on('albums');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
