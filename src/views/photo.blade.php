7481c4f62c7ceb4f364676b641296648fbf918e8
JeroenG <jeroengjeroeng@gmail.com>
2013-07-14 19:57:23 +0200

    Return url to album page instead of gallery home

--- src/views/photo.blade.php
+++ src/views/photo.blade.php
@@ -1,6 +1,6 @@
 @section('content')
 	
-	<a href="{{ URL::to('gallery') }}">{{ Lang::get('gallery::gallery.return') }}</a>
+	<a href='{{ URL::to("gallery/album/$photo->album_id") }}'>{{ Lang::get('gallery::gallery.return') }}</a>
 
 	<h1>{{ $photo->photo_name }}</h1>
 
