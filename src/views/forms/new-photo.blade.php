7691dff3e08dee85c2c412229d0bf20a6f598acf
JeroenG <jeroengjeroeng@gmail.com>
2013-07-14 21:45:18 +0200

    Renamed according to validation rules

--- src/views/forms/new-photo.blade.php
+++ src/views/forms/new-photo.blade.php
@@ -1,23 +1,23 @@
 {{ Form::open(array('url' => 'gallery/photo', 'method' => 'POST', 'files' => true)) }}
     <ul>
         <li>
-            {{ Form::label('name', Lang::get('gallery::gallery.name') . ':') }}
-            {{ Form::text('name', null, $options = array('size'=>'50')) }}
+            {{ Form::label('photo_name', Lang::get('gallery::gallery.name') . ':') }}
+            {{ Form::text('photo_name', null, $options = array('size'=>'50')) }}
         </li>
 
         <li>
-            {{ Form::label('path', Lang::get('gallery::gallery.path') . ':') }}
-            {{ Form::file('path') }}
+            {{ Form::label('photo_path', Lang::get('gallery::gallery.path') . ':') }}
+            {{ Form::file('photo_path') }}
         </li>
 
         <li>
-            {{ Form::label('album', Lang::get('gallery::gallery.album') . ':') }}
-            {{ Form::select('album', $dropdown) }}
+            {{ Form::label('album_id', Lang::get('gallery::gallery.album') . ':') }}
+            {{ Form::select('album_id', $dropdown) }}
         </li>
 
         <li>
-            {{ Form::label('description', Lang::get('gallery::gallery.desc') . ':') }}
-            {{ Form::textarea('description', null, $options = array('size'=>'50x5')) }}
+            {{ Form::label('photo_description', Lang::get('gallery::gallery.desc') . ':') }}
+            {{ Form::textarea('photo_description', null, $options = array('size'=>'50x5')) }}
         </li>
 
         <li>
