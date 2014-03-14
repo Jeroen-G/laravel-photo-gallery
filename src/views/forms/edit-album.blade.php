9f09a34126eb42057e80ba4b41d2a38f2dc71264
JeroenG <jeroengjeroeng@gmail.com>
2014-03-14 16:40:42 +0100

    Updated: style of the forms

--- src/views/forms/edit-album.blade.php
+++ src/views/forms/edit-album.blade.php
@@ -1,18 +1,18 @@
 {{ Form::model($album, array('method' => 'PUT', 'route' => array('gallery.album.update', $album->album_id))) }}
-    <ul>
-        <li>
-            {{ Form::label('album_name', Lang::get('gallery::gallery.name') . ':') }}
-            {{ Form::text('album_name', null, $options = array('size'=>'50')) }}
-        </li>
 
-        <li>
-            {{ Form::label('album_description', Lang::get('gallery::gallery.desc') . ':') }}
-            {{ Form::text('album_description', null, $options = array('size'=>'30')) }}
-        </li>
+    <div class="form-group">
+        {{ Form::label('album_name', Lang::get('gallery::gallery.name') . ':') }}
+        {{ Form::text('album_name', null, $options = array('class' => 'form-control')) }}
+    </div>
 
-        <li>
-            {{ Form::submit(Lang::get('gallery::gallery.submit'), array('class' => 'btn btn-primary')) }}
-            {{ link_to_route("gallery.album.show", Lang::get('gallery::gallery.cancel'), array($album->album_id), array('class' => 'btn btn-default')) }}
-        </li>
-    </ul>
+    <div class="form-group">
+        {{ Form::label('album_description', Lang::get('gallery::gallery.desc') . ':') }}
+        {{ Form::text('album_description', null, $options = array('class' => 'form-control')) }}
+    </div>
+
+    <div class="form-group">
+        {{ Form::submit(Lang::get('gallery::gallery.submit'), array('class' => 'btn btn-primary')) }}
+        {{ link_to_route("gallery.album.show", Lang::get('gallery::gallery.cancel'), array($album->album_id), array('class' => 'btn btn-default')) }}
+    </div>
+
 {{ Form::close() }}
