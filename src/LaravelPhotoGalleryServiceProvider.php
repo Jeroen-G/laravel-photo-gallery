d67e83296440cc64a605e564877f0685b94a399f
Jeroen-G <jeroengjeroeng@gmail.com>
2015-03-04 17:30:08 +0100

    Add migrations

--- src/LaravelPhotoGalleryServiceProvider.php
+++ src/LaravelPhotoGalleryServiceProvider.php
@@ -4,61 +4,62 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelPhotoGalleryServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$resources = realpath(__DIR__.'/../resources');
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $resources = realpath(__DIR__.'/../resources');
 
-		$this->loadViewsFrom($resources.'/views', 'gallery');
+        $this->loadViewsFrom($resources.'/views', 'gallery');
         $this->loadTranslationsFrom($resources.'/lang', 'gallery');
 
-		$this->publishes([
-            $resources.'/views' => base_path('resources/views/vendor/gallery'),
+        $this->publishes([
+            $resources.'/views'              => base_path('resources/views/vendor/gallery'),
             $resources.'/config/gallery.php' => config_path('gallery.php'),
-            __DIR__.'/../public' => public_path('gallery'),
-    	]);
+            $resources.'/migrations'         => $this->app->databasePath().'/migrations',
+            __DIR__.'/../public'             => public_path('gallery'),
+        ]);
 
         include $resources.'/routes.php';
-	}
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$resources = realpath(__DIR__.'/../resources');
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $resources = realpath(__DIR__.'/../resources');
         $this->mergeConfigFrom($resources.'/config/gallery.php', 'gallery');
 
-		if (file_exists(app_path().'/bindings.php')) {
-			include app_path() . '/bindings.php';
-		}
-		else
-		{
-			include $resources.'/bindings.php';
-		}
-	}
+        if (file_exists(app_path().'/bindings.php')) {
+            include app_path() . '/bindings.php';
+        }
+        else
+        {
+            include $resources.'/bindings.php';
+        }
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return ['gallery'];
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['gallery'];
+    }
 
 }
