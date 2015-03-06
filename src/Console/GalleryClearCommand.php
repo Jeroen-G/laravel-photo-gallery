37e530529252c71822001abc06296cb4e55483cb
Jeroen-G <jeroengjeroeng@gmail.com>
2015-03-06 16:54:17 +0100

    Working Artisan command to easily clear gallery

--- src/Console/GalleryClearCommand.php
+++ src/Console/GalleryClearCommand.php
@@ -1,6 +1,9 @@
 <?php namespace JeroenG\LaravelPhotoGallery\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Filesystem\Factory as Filesystem;
+use JeroenG\LaravelPhotoGallery\Contracts\AlbumRepository;
+use JeroenG\LaravelPhotoGallery\Contracts\PhotoRepository;
 
 class GalleryClearCommand extends Command {
 
@@ -23,9 +26,12 @@ class GalleryClearCommand extends Command {
 	 *
 	 * @return void
 	 */
-	public function __construct()
+	public function __construct(AlbumRepository $albums, PhotoRepository $photos, Filesystem $storage)
 	{
 		parent::__construct();
+		$this->photos = $photos;
+		$this->albums = $albums;
+		$this->storage = $storage;
 	}
 
 	/**
@@ -36,8 +42,19 @@ class GalleryClearCommand extends Command {
 	public function fire()
 	{
 		$this->comment('Deleting photos...');
-		
+		$allPhotos = $this->photos->all();
+		foreach ($allPhotos as $photo) {
+			$this->photos->forceDelete($photo->photo_id, $this->storage);
+		}
+
 		$this->comment('Deleting albums...');
+		$allAlbums = $this->albums->all();
+		foreach ($allAlbums as $album) {
+			if($album->album_id != 1)
+			{
+				$this->albums->forceDelete($album->album_id, $this->photos, $this->storage);
+			}
+		}
 
 		$this->info('Gallery cleared!');
 	}
