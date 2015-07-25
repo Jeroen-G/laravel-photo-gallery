8213fca61b2854b7dc7436c22c0e6922c2899523
JeroenG <jeroengjeroeng@gmail.com>
2015-07-25 19:10:55 +0200

    Photo and Album entities.

---
+++ src/Entities/Album.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace JeroenG\LaravelPhotoGallery\Entities;
+
+use Illuminate\Support\Collection;
+use JeroenG\LaravelPhotoGallery\Contracts;
+use JeroenG\LaravelPhotoGallery\Traits\Editable;
+use JeroenG\LaravelPhotoGallery\Traits\Mappable;
+
+class Album implements Contracts\Album
+{
+    use Mappable, Editable;
+
+    private $metadata;
+    private $photos;
+
+    public function getId()
+    {
+        return $this->metadata['id'];
+    }
+
+    public function getName()
+    {
+        return $this->metadata['name'];
+    }
+
+    public function getDescription()
+    {
+        return $this->metadata['description'];
+    }
+
+    public function getMetadata($attribute)
+    {
+        return $this->metadata[$attribute];
+    }
+
+    public function metadataContains($attribute)
+    {
+        return isset($this->metadata[$attribute]);
+    }
+
+    public function getPhotos()
+    {
+        return Collection::make($this->photos);
+    }
+
+    public function addPhotos($data)
+    {
+        if (is_array($data)) {
+            foreach ($data as $photo) {
+                $this->addPhoto($photo);
+            }
+        }
+    }
+
+    public function addPhoto(Contracts\Photo $photo)
+    {
+        $this->photos[$photo->getId()] = $photo;
+    }
+
+    public function removePhoto(Contracts\Photo $photo)
+    {
+        if(array_key_exists($photo->getId(), $this->photos)) {
+            unset($this->photos[$photo->getId()]);
+        }
+    }
+
+    public function rename($newName)
+    {
+        return $this->metadata['name'] = $newName;
+    }
+}
---
+++ src/Entities/Photo.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace JeroenG\LaravelPhotoGallery\Entities;
+
+use JeroenG\LaravelPhotoGallery\Contracts;
+use JeroenG\LaravelPhotoGallery\Traits\Editable;
+use JeroenG\LaravelPhotoGallery\Traits\Mappable;
+
+class Photo implements Contracts\Photo
+{
+    use Mappable, Editable;
+
+    private $metadata;
+
+    public function getId()
+    {
+        return $this->metadata['id'];
+    }
+
+    public function getName()
+    {
+        return $this->metadata['name'];
+    }
+
+    public function getDescription()
+    {
+        return $this->metadata['description'];
+    }
+
+    public function getFile()
+    {
+        return $this->metadata['file'];
+    }
+
+    public function getSize()
+    {
+        return $this->metadata['size'];
+    }
+
+    public function getAlbum()
+    {
+        return $this->metadata['album'];
+    }
+
+    public function getMetadata($attribute)
+    {
+        return $this->metadata[$attribute];
+    }
+
+    public function metadataContains($attribute)
+    {
+        return isset($this->metadata[$attribute]);
+    }
+}
